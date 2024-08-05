3624bdd4ca5f1ab36057ddb296d3a3aec8b9d3e9
wasabina67 <wasabina67@gmail.com>
2024-08-05 22:24:40 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-import ollama  # noqa
+import ollama
 
 
 def main():
-    pass
+    content = "なぜ空は青いのですか？"
+    resp = ollama.chat(
+        model="llama3.1", messages=[{"role": "user", "content": content}]
+    )
+    print(resp["message"]["content"])
 
 
 if __name__ == "__main__":
